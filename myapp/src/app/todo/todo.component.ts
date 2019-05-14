a521b0873deaa167b01baae9ed7eb80b7678acba
pedrobarata <pedrofelipebarata@gmail.com>
2019-05-13 23:16:16 -0300

    feat(video 3 10min): Subscribers

--- myapp/src/app/todo/todo.component.ts
+++ myapp/src/app/todo/todo.component.ts
@@ -8,47 +8,57 @@ import { APIService } from '../API.service';
 export class TodoComponent implements OnInit {
 
   allTodos: any = [];
- /*  timer: number = 0;
-  isStarted: boolean = false;
-  interval: NodeJS.Timer */
+  /*  timer: number = 0;
+   isStarted: boolean = false;
+   interval: NodeJS.Timer */
   constructor(private api: APIService) { }
 
   async ngOnInit() {
     let result = await this.api.ListTodos();
     this.allTodos = result.items;
-  }
-/* 
-  setTimer() {
-    this.isStarted = true;
-    this.interval = setInterval(() => {
-      this.timer++;
-      if(this.timer >= 5) {
-        clearInterval(this.interval);
-        this.isStarted = false;
-        console.log("asd");
-        this.timer = 0;
+
+    this.api.OnCreateTodoListener.subscribe({
+      next: (todo) => {
+        console.log(todo);
+        
+        this.allTodos.push(todo.value.data.onCreateTodo);
       }
-    }, 1000)
+    })
   }
-
-  pauseTimer() {
-    clearInterval();
-    this.isStarted = false;
-    console.log("paused!");
-    this.timer = 0;
-  } */
-
-  async createTodo() {
-    const newTodo = {
-      name: 'Todo' + Math.floor(Math.random() * 100),
-      body: 'sample description',
-      completed: false
+  /* 
+    setTimer() {
+      this.isStarted = true;
+      this.interval = setInterval(() => {
+        this.timer++;
+        if(this.timer >= 5) {
+          clearInterval(this.interval);
+          this.isStarted = false;
+          console.log("asd");
+          this.timer = 0;
+        }
+      }, 1000)
     }
+  
+    pauseTimer() {
+      clearInterval();
+      this.isStarted = false;
+      console.log("paused!");
+      this.timer = 0;
+    } */
 
-    let result = await this.api.CreateTodo(newTodo);
+  async createTodo(todoName) {
+    if (todoName.value) {
+      const newTodo = {
+        name: todoName.value,
+        body: 'sample description',
+        completed: false
+      }
 
-    this.allTodos.push(result);
-    debugger;
+      let result = await this.api.CreateTodo(newTodo);
+     /*  this.allTodos.push(result); */
+      todoName.value = null;
+      // debugger;
+    }
 
 
   }
