6e56f2db00e975e0c62b84af777825daf6183e2f
pedrobarata <pedrofelipebarata@gmail.com>
2019-05-13 23:01:48 -0300

    Teste com o timer (comentado)

--- myapp/src/app/todo/todo.component.ts
+++ myapp/src/app/todo/todo.component.ts
@@ -8,12 +8,35 @@ import { APIService } from '../API.service';
 export class TodoComponent implements OnInit {
 
   allTodos: any = [];
+ /*  timer: number = 0;
+  isStarted: boolean = false;
+  interval: NodeJS.Timer */
   constructor(private api: APIService) { }
 
   async ngOnInit() {
     let result = await this.api.ListTodos();
     this.allTodos = result.items;
   }
+/* 
+  setTimer() {
+    this.isStarted = true;
+    this.interval = setInterval(() => {
+      this.timer++;
+      if(this.timer >= 5) {
+        clearInterval(this.interval);
+        this.isStarted = false;
+        console.log("asd");
+        this.timer = 0;
+      }
+    }, 1000)
+  }
+
+  pauseTimer() {
+    clearInterval();
+    this.isStarted = false;
+    console.log("paused!");
+    this.timer = 0;
+  } */
 
   async createTodo() {
     const newTodo = {
